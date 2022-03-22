b2920666e4bcf845f00d68fc36c7e7cd5985449c
Ishaan3030 <86636860+Ishaan3030@users.noreply.github.com>
2022-03-22 07:26:38 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,8 @@
+import React,{Component} from 'react';
+import BottomTabNavigator from './components/BottomTabNavigator';
+
+export default class App extends React.Component{
+  render(){
+    return<BottomTabNavigator/>;
+  }
+}
